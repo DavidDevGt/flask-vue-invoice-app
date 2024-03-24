082e2ff05b006c97b550111656153c387d8da25b
DavidDevGt <josuedavidvl18@gmail.com>
2024-03-24 12:20:47 -0600

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
