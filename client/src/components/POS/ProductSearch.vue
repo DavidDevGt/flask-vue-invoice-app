d97e94496a04320ed08a62368b1ada9aff598c7e
DavidDevGt <josuedavidvl18@gmail.com>
2024-03-31 23:12:48 -0600

    componente para buscar productos

---
+++ client/src/components/POS/ProductSearch.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container py-5">
+        <div class="product-search">
+            <div class="search-options mb-3">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Buscar producto" aria-label="Buscar producto">
+                    <button class="btn btn-outline-secondary" type="button">Buscar</button>
+                </div>
+                <div class="row g-3">
+                    <div class="col-md">
+                        <select class="form-select" aria-label="Categoría">
+                            <option selected>Todas las categorías</option>
+                        </select>
+                    </div>
+                    <div class="col-md">
+                        <select class="form-select" aria-label="Subcategoría">
+                            <option selected>Todas las subcategorías</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
